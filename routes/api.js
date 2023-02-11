ce7d29aaf6e99a084aa761a1ed3e1c4992556efc
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-11 10:36:31 +0000

    2023年02月11日 - 03

--- routes/api.js
+++ routes/api.js
@@ -4,24 +4,24 @@ module.exports = function (app) {
   
   app.route('/api/threads/:board')
     .post(function(req, res) {
-      //
+      console.dir(req);
     })
     .put(function(req, res) {
-      //
+      console.dir(req);
     })
     .delete(function(req, res) {
-      //
+      console.dir(req);
     });
   
   app.route('/api/replies/:board')
     .post(function(req, res) {
-      //
+      console.dir(req);
     })
     .put(function(req, res) {
-      //
+      console.dir(req);
     })
     .delete(function(req, res) {
-      //
+      console.dir(req);
     });
 
 };
